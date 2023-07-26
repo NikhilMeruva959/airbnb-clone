bf9c69e3b28f64a619141602a82eeeb267052066
Nikhil Meruva <nikmeruva1@gmail.com>
2023-07-26 11:05:37 -0400

    Input and heading component created

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -2,18 +2,18 @@
 
 import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
-// import { signIn } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import { useCallback, useState } from "react";
-// import { toast } from "react-hot-toast";
+import { toast } from "react-hot-toast";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import useLoginModal from "@/app/hooks/useLoginModal";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 import Modal from "./Modal";
-// import Input from "../inputs/Input";
-// import Heading from "../Heading";
+import Input from "../inputs/Input";
+import Heading from "../Heading";
 import Button from "../Button";
 
 const RegisterModal = () => {
